0e85a5eeb059978254c029f5c2ba0354b14973d0
MGilangSeftian <gilangseftian90@gmail.com>
2024-10-21 13:06:19 +0700

    progress commit

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'screens/detail_screen.dart';
+import 'package:wisata_candi/models/candi.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,18 +20,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      // home: DetailScreen(candi: candiList[0]),
+      home: DetailScreen(candi: candiList[0]),
     );
   }
 }
-class DetailScreen extends StatelessWidget{
-  const DetailScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
 
 
 
