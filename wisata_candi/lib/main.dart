8f5e44727f6cf17b3cf1c55551659144983db945
MGilangSeftian <143242901+MGilangSeftian@users.noreply.github.com>
2024-10-22 20:08:23 +0700

    update pertemuan 12
    update project wisata candi pertemuan 12

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
-import 'screens/detail_screen.dart';
-import 'package:wisata_candi/models/candi.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+
 
 
 void main() {
